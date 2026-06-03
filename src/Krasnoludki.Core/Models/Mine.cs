6dcf56e0df59c173abac8ecb677cd1e5bd5c8fb2
Michal Janiec <michaljaniec12@wp.pl>
2026-06-03 14:17:14 +0200

    another fix

--- src/Krasnoludki.Core/Models/Mine.cs
+++ src/Krasnoludki.Core/Models/Mine.cs
@@ -1,19 +1,24 @@
 namespace Krasnoludki.Core.Models;
 
-public class Mine
+public class Mine : Point
 {
-    private static int _MineCounter = 1;
-    public int Id{ get; }
-    public Point Location{ get; }
     public MineralType Resource{ get; }
     public int Capacity{ get; }
+    public List<Dwarf> Workers;     //lista na Id pracujących w kopalni krasnoludków
+    public bool IsFull;     //czy kopalnia ma maks pracowników
 
-    public Mine(double x, double y, MineralType mineral, int capacity)
+    public Mine(int id, double x, double y, MineralType mineral, int capacity) : base(id, x, y)
     {
-        Id = _MineCounter++;
-        Location = new Point(x,y);
         Resource = mineral;
         Capacity = capacity;
+        Workers = new List<Dwarf>();
+        IsFull = false;
+    }
+
+    public void AddWorker(Dwarf dwarf)
+    {
+        Workers.Add(dwarf);
+        if(Workers.Count >= Capacity) IsFull = true;
     }
 
 }
